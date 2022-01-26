d7b6002b685798c6e34e72ac1a8b5168cc38adf3
Pushkar Singh <stoazest@gmail.com>
2022-01-26 22:01:10 +0530

    Signup page done

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request");
+const { response } = require("express");
+
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(express.static("public"));
+app.use(express.static(__dirname));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/signup.html");
+})
+
+app.post("/", function(req, res){
+    const name = req.body.name;
+    const email = req.body.email;
+    const password  = req.body.password;
+
+    console.log("Name: " + name + " email: " + email + " password " + password);
+;})
 
 app.listen(port, function () {
     console.log("server is running on port " + port);
