a741062af2e17fde06bb6881167f26228afd0258
Pushkar Singh <stoazest@gmail.com>
2022-01-26 16:05:18 +0530

    Basic Dependencies setup done

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.listen(port, function () {
+    console.log("server is running on port " + port);
+})
